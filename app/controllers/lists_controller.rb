205e59f3a3012403b6000fbe1f7c20d8b48947b8
github username <12ivazquez@gmail.com>
2017-10-14 21:17:14 -0500

    Lists can be created, edited, deleted by writers

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,8 +12,44 @@ class ListsController < ApplicationController
     if params[:title].empty?
       redirect '/lists/new'
     else
-      current_user.lists.create(:title => params[:title])
+      list = current_user.lists.create(:title => params[:title])
+      list_item = ListItem.create(:description => params[:list_items][:description], :word_number => params[:list_items][:word_number])
+      list.list_items << list_item
       redirect '/'
+    end
+  end
+
+  get '/lists/:id/edit' do
+    @list = List.find_by_id(params[:id])
+    if logged_in?
+      if @list.writer_id == current_user.id
+        erb :'lists/edit'
+      else
+        redirect '/'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/lists/:id/edit' do
+    @list = List.find_by_id(params[:id])
+    if @list.writer_id = session[:writer_id]
+      @list.title = params[:title]
+      @list.save
+      redirect '/'
+    else
+      redirect '/'
+    end
+  end
+
+  delete '/lists/:id/delete' do
+    @list = List.find_by_id(params[:id])
+    if logged_in? && (current_user.id == @list.writer_id)
+      @list.delete
+      redirect '/'
+    else
+      redirect "/login"
     end
   end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,7 @@
 class List < ActiveRecord::Base
   has_many :list_items
   belongs_to :writer
+
+  validates :title, presence: true
+  validates :writer_id, presence: true
 end
